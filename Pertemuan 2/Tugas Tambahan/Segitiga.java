b781bb5172c7c00cb36cc0d81b301333311dc08c
Keisya Intan Nabila <128314097+Keisya31@users.noreply.github.com>
2024-03-02 10:54:33 +0700

    Add files via upload

---
+++ Pertemuan 2/Tugas Tambahan/Segitiga.java
@@ -0,0 +1,28 @@
+/*Nama: Keisya Intan Nabila
+ * NIM: 24060122130063
+ * Lab: B2
+ * Tanggal: Jumat, 01 Maret 2024
+ * Program: Membuat body program untuk Segitiga
+ */
+public class Segitiga {
+    private double alas;
+    private double tinggi;
+
+    public Segitiga (double alas, double tinggi){
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    public double getAlas(){
+        return alas;
+    }
+
+    public double getTinggiSegitiga(){
+        return tinggi;
+    }
+
+    public double hitungLuas(){
+        return 0.5 * getAlas() * getTinggiSegitiga();
+    }
+
+}
